9ea71924d045326ccbf071aa16b78b577881747c
КДБ <prykhozhdenko@gmail.com>
2026-05-29 09:07:31 +0300

    початок

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+VERSION="$1"
+
+if [ -z "$VERSION" ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+
+EXTRACT_DIR="$(mktemp -d)"
+OUTPUT_DIR="./ціль-$VERSION"
+ARCHIVE_NAME="ціль-$VERSION.tar.xz"
+
+DOWNLOAD_URL="https://github.com/tsil-ukr/files/raw/main/%D0%B2%D0%B8%D0%BF%D1%83%D1%81%D0%BA%D0%B8-%D1%86%D1%96%D0%BB%D1%96/$VERSION/%D1%86%D1%96%D0%BB%D1%8C-$VERSION.tar.xz"
+
+wget -O "$ARCHIVE_NAME" "$DOWNLOAD_URL"
+
+mkdir -p "$OUTPUT_DIR"
+
+tar -xf "$ARCHIVE_NAME" -C "$EXTRACT_DIR"
+
+докс код --вхід="$EXTRACT_DIR/ціль-$VERSION" --вихід="$OUTPUT_DIR" --назва="ціль-$VERSION" --завантажити="$DOWNLOAD_URL" --гітхаб="https://github.com/tsil-ukr/tsil"
+
+cp "favicon.png" "$OUTPUT_DIR/докс_код_favicon.png"
+cp "favicon.svg" "$OUTPUT_DIR/докс_код_favicon.svg"
